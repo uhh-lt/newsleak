4263d0a1478bdbedaff4c2e55c0e297931648dd8
Florian Zouhar <florian.zouhar@gris.tu-darmstadt.de>
2016-07-14 13:55:52 +0200

    more scala like code

--- app/util/TimeRangeParser.scala
+++ app/util/TimeRangeParser.scala
@@ -26,23 +26,22 @@ import org.joda.time.format.DateTimeFormat
 case class TimeRange(from: Option[LocalDateTime], to: Option[LocalDateTime])
 
 object TimeRangeParser {
+  private val YearRange = "(\\d{4})-(\\d{4})".r
+  private val Year = "(\\d{4})".r
+  private val Month = "([A-Z][a-z]*) (\\d{4})".r
+  private val Date = "(\\d{1,2}).(\\d{1,2}).(\\d{4})".r
 
-  def parseTimeRange(range: String): TimeRange = {
-    if (range.isEmpty) {
-      TimeRange(None, None)
-    } else if (range.matches("[0-9]{4}-[0-9]{4}")) {
-      val yearRange = range.split("-")
-      TimeRange(Some(LocalDateTime.parse(yearRange(0), DateTimeFormat.forPattern("yyyy"))), Some(LocalDateTime.parse(s"31.12.${yearRange(1)}", DateTimeFormat.forPattern("dd.MM.yyyy"))))
-    } else if (range.matches("[0-9]{4}")) {
-      TimeRange(Some(LocalDateTime.parse(range, DateTimeFormat.forPattern("yyyy"))), Some(LocalDateTime.parse(s"31.12.${range}", DateTimeFormat.forPattern("dd.MM.yyyy"))))
-    } else if (range.matches("[A-Z][a-z]* [0-9]{4}")) {
-      val month = LocalDateTime.parse(range, DateTimeFormat.forPattern("MMMMM yyyy"))
-      TimeRange(Some(month), Some(month.dayOfMonth().withMaximumValue()))
-    } else if (range.matches("[0-9]{1,2}.[0-9]{1,2}.[0-9]{4}")) {
-      val date = Some(LocalDateTime.parse(range, DateTimeFormat.forPattern("dd.MM.yyyy")))
+  def parseTimeRange(range: String): TimeRange = range match {
+    case YearRange(from, to) =>
+      TimeRange(Some(LocalDateTime.parse(from, DateTimeFormat.forPattern("yyyy"))), Some(LocalDateTime.parse(s"31.12.${to}", DateTimeFormat.forPattern("dd.MM.yyyy"))))
+    case Year(year) =>
+      TimeRange(Some(LocalDateTime.parse(year, DateTimeFormat.forPattern("yyyy"))), Some(LocalDateTime.parse(s"31.12.${year}", DateTimeFormat.forPattern("dd.MM.yyyy"))))
+    case Month(month, year) =>
+      val monthYear = LocalDateTime.parse(s"$month-$year", DateTimeFormat.forPattern("MMMMM-yyyy"))
+      TimeRange(Some(monthYear), Some(monthYear.dayOfMonth().withMaximumValue()))
+    case Date(day, month, year) =>
+      val date = Some(LocalDateTime.parse(s"$day-$month-$year", DateTimeFormat.forPattern("dd-MM-yyyy")))
       TimeRange(date, date)
-    } else {
-      TimeRange(None, None)
-    }
+    case _ => TimeRange(None, None)
   }
 }
