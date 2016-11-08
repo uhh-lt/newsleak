5ef2e4329a1a3a3cebe24ef201ba35766533baff
Uli Fahrer <mail@uli-fahrer.de>
2016-11-08 10:21:50 +0100

    Add type information to merge functionality

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -228,7 +228,7 @@ define([
             }
 
             $scope.joinDuplicates = function(duplicates) {
-                return duplicates.map(function(d) { return d.name; }).join(', ')
+                return duplicates.map(function(d) { return '' + d.name + ' (' + d.type + ')'; }).join(', ')
             };
 
             $scope.toggle = function (item, list) {
