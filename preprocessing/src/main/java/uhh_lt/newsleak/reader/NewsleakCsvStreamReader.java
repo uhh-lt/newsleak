34c33455a9c0591091e0e2fad22ff347f3f3c609
Gregor Wiedemann <kontakt@gregor-wiedemann.de>
2019-03-26 18:36:00 +0100

    documentation

--- preprocessing/src/main/java/uhh_lt/newsleak/reader/NewsleakCsvStreamReader.java
+++ preprocessing/src/main/java/uhh_lt/newsleak/reader/NewsleakCsvStreamReader.java
@@ -20,42 +20,68 @@ import org.apache.uima.util.ProgressImpl;
 
 import uhh_lt.newsleak.types.Metadata;
 
+/**
+ * The Class NewsleakCsvStreamReader assumes externally generated Ids.
+ */
 public class NewsleakCsvStreamReader extends NewsleakReader {
 
-	/**
-	 * Directory containing input files
-	 */
+	/** Directory containing input files. */
 	public static final String PARAM_INPUTDIR = "inputDir";
+	
+	/** The input dir. */
 	@ConfigurationParameter(name=PARAM_INPUTDIR, mandatory=false, defaultValue=".")
 	private String inputDir;
 
+	/** The Constant PARAM_DOCUMENT_FILE. */
 	public static final String PARAM_DOCUMENT_FILE = "documentFile";
+	
+	/** The document file. */
 	@ConfigurationParameter(name=PARAM_DOCUMENT_FILE, mandatory=true)
 	private String documentFile;
 
+	/** The Constant PARAM_METADATA_FILE. */
 	public static final String PARAM_METADATA_FILE = "metadataFile";
+	
+	/** The metadata file. */
 	@ConfigurationParameter(name=PARAM_METADATA_FILE, mandatory=true)
 	private String metadataFile;
 
+	/** The Constant PARAM_DEFAULT_LANG. */
 	public static final String PARAM_DEFAULT_LANG = "defaultLanguage";
+	
+	/** The default language. */
 	@ConfigurationParameter(name=PARAM_DEFAULT_LANG, mandatory=false, defaultValue="en")
 	private String defaultLanguage;
 	
+	/** The Constant PARAM_DEBUG_MAX_DOCS. */
 	public static final String PARAM_DEBUG_MAX_DOCS = "maxRecords";
+	
+	/** The max records. */
 	@ConfigurationParameter(name=PARAM_DEBUG_MAX_DOCS, mandatory=false)
 	private Integer maxRecords = Integer.MAX_VALUE;
 
+	/** The csv reader. */
 	private Reader csvReader;
+	
+	/** The records. */
 	private Iterable<CSVRecord> records;
+	
+	/** The records iterator. */
 	private Iterator<CSVRecord> recordsIterator;
 	
 //	private Reader metadataReader;
 //	private Iterable<CSVRecord> metadata; 
 //	private Iterator<CSVRecord> metadataIterator; 
 
-	private int totalRecords = 0;
+	/** The total records. */
+private int totalRecords = 0;
+	
+	/** The current record. */
 	private int currentRecord = 0;
 
+	/* (non-Javadoc)
+	 * @see org.apache.uima.fit.component.CasCollectionReader_ImplBase#initialize(org.apache.uima.UimaContext)
+	 */
 	@Override
 	public void initialize(UimaContext context) throws ResourceInitializationException {
 		super.initialize(context);
@@ -75,6 +101,9 @@ public class NewsleakCsvStreamReader extends NewsleakReader {
 	}
 
 
+	/* (non-Javadoc)
+	 * @see org.apache.uima.collection.CollectionReader#getNext(org.apache.uima.cas.CAS)
+	 */
 	public void getNext(CAS cas) throws IOException, CollectionException {
 		currentRecord++;
 		JCas jcas;
@@ -86,11 +115,7 @@ public class NewsleakCsvStreamReader extends NewsleakReader {
 		
 		// Set document data
 		CSVRecord record = recordsIterator.next();
-		String docId = record.get(0);
-		
-		// split into paragraphs
-		// add paragraph with annotation
-		// LangDect: per paragraph, doc-lang = majority of paragraph languages
+		String docId = record.get(0); // external document id from CSV file
 		
 		jcas.setDocumentText(cleanBodyText(record.get(1)));
 		jcas.setDocumentLanguage(record.size() > 3 ? record.get(3) : defaultLanguage);
@@ -101,27 +126,23 @@ public class NewsleakCsvStreamReader extends NewsleakReader {
 		metaCas.setTimestamp(record.get(2));
 		metaCas.addToIndexes();
 		
-//		CSVRecord metaRecord;
-//		ArrayList<String> tripletNames = new ArrayList<String>();
-//		ArrayList<String> tripletValues = new ArrayList<String>();
-//		ArrayList<String> tripletTypes = new ArrayList<String>();
-//		while (metadataIterator.hasNext()) {
-//			metaRecord = metadataIterator.next();
-//			Integer refId = Integer.parseInt(metaRecord.get(2));
-//			if (docId != refId) {
-//				break;
-//			}
-//		}
-		
+		// metadata
+		// is assumed to be provided from external prcessing in a separate file
 		
 	}
 
 
+	/* (non-Javadoc)
+	 * @see org.apache.uima.fit.component.CasCollectionReader_ImplBase#close()
+	 */
 	public void close() throws IOException {
 		csvReader.close();
 		// metadataReader.close();
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.uima.collection.base_cpm.BaseCollectionReader#getProgress()
+	 */
 	public Progress[] getProgress() {
 		return new Progress[] {
 				new ProgressImpl(
@@ -132,6 +153,9 @@ public class NewsleakCsvStreamReader extends NewsleakReader {
 		};
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.uima.collection.base_cpm.BaseCollectionReader#hasNext()
+	 */
 	public boolean hasNext() throws IOException, CollectionException {
 		if (currentRecord > maxRecords) return false;
 		return recordsIterator.hasNext();
