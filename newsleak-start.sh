1cece5ceeeb9fa25603fffe1add04b6658ea038a
Gregor Wiedemann <kontakt@gregor-wiedemann.de>
2019-04-25 16:35:00 +0200

    copy data dir to volume

--- newsleak-start.sh
+++ newsleak-start.sh
@@ -2,6 +2,7 @@
 cd /opt/newsleak
 rm -f RUNNING_PID
 cp -n -r conf /etc/settings/
-chmod -R 777 /etc/settings/conf
+cp -n -r data /etc/settings/
+chmod -R 777 /etc/settings/
 export NEWSLEAK_CONFIG=/etc/settings/conf/application.production.conf
 bin/newsleak -Dconfig.file=/etc/settings/conf/application.production.conf
